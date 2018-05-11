55e32e6a92a64782f99250cf09e1290de7d60496
nkean <nathankean2@gmail.com>
2018-05-11 10:52:23 -0500

    add form to genre view for adding new genre

--- server/public/scripts/services/genre.service.js
+++ server/public/scripts/services/genre.service.js
@@ -1,4 +1,32 @@
 app.service('GenreService', ['$http', function($http) {
     console.log('GenreService has loaded');
     var self = this;
+
+    self.newGenre = {
+        name: '',
+    };
+
+    self.genres = {
+        list: [],
+    };
+
+    self.defaultInput = function() {
+        self.newGenre.name = '';
+    }
+
+    self.addGenre = function() {
+        console.log('Adding to database: ', self.newGenre);
+        $http({
+            method: 'POST',
+            url: '/genre/add',
+            data: self.newGenre,
+        })
+        .then(function(response) {
+            console.log('Successful POST: ', response);
+        })
+        .catch(function(error) {
+            console.log('Error with POST: ', error);
+        })
+    }
+    
 }]);
